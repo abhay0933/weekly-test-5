b75e1d4bb9881dc0e41725c7d7c611a2c57db8b7
abhay0933 <abhay80912@gmail.com>
2023-10-24 12:25:08 +0530

    Some minor changes done

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const loadData = async()=>{
-    const res =await fetch('https://openapi.programming-hero.com/api/phones?search=oppo');
-    const data = await res.json();  
-    const phone = data.data;
-    console.log(phone);
-}
-loadData();
+let phoneSearch = document.querySelector(".input-search");
+let searchBtn = document.querySelector(".search");
+let defaultphones = document.querySelectorAll(".item");
+let phonecontainer = document.querySelector(".showItem-container");
+
+searchBtn.addEventListener("click", () => {
+  // console.log(phoneSearch.value);
+  let phoneValue = fetch(
+    (url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearch.value}`)
+  );
+  // console.log(phoneValue);
+  phoneValue
+
+    .then(data => {
+      let jsonn = data.json();
+      // console.log(jsonn)
+      return jsonn;
+    })
+
+    .then(item => {
+      console.log(item.data);
+      num = 20;
+      for (let i = 0; i < defaultphones.length; i++) {
+        defaultphones[i].style.display = "none";
+      }
+
+      for (let i = 0; i <= num; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
+        // console.log(newDiv);
+        newDiv.innerHTML = `
+                <img src=${item.data[i].image} alt="" />
+                <div class="content">
+                <h2>${item.data[i].phone_name}</h2>
+                <p>
+                  There are many variations of passages of available, but the
+                  majority have suffered
+                </p>
+                <button class="detailsbtn">SHOW DETAILS</button>
+                </div>
+                 `;
+
+        phonecontainer.appendChild(newDiv);
+      }
+    });
+});
+
+let deatilsBtn= document.querySelectorAll(".detailsbtn");
+    console.log(deatilsBtn);
+  
+
+
